ee01a184b7b2c541de36133932fac5227e869ac5
annagwozdz <zuza.gwo@gmail.com>
2024-09-03 17:04:01 +0100

    Posts View page with mock data

--- MobileAcebook/Views/PostsView.swift
+++ MobileAcebook/Views/PostsView.swift
@@ -3,43 +3,101 @@
 //  MobileAcebook
 //
 //  Created by Avni Sualy on 03/09/2024.
-//
-
 import SwiftUI
-struct PostsView:View {
-    // @State var
+
+struct PostsView: View {
+    let post: Post
+    
+    var welcome: some View {
+        VStack {
+            Text("Welcome User")
+                .padding(20)
+                .font(.largeTitle)
+                .padding(.bottom, 20)
+//            Text("Recent Posts")
+//                .padding(.leading, 20)
+//                .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+    
+    var header: some View {
+        HStack {
+            Image("profile")
+            // Image(post.imgUrl)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .clipped()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .padding(.leading)
+            
+            VStack(alignment: .leading) {
+                Text(post.username)
+                    .font(.system(size: 30))
+                    .bold()
+            }
+            
+            Spacer()
+            
+            Image(systemName: "ellipsis")
+                .padding()
+        }
+    }
+    
+    var actionButtons: some View {
+        HStack {
+            Image(systemName: "heart")
+                .renderingMode(.template)
+                .foregroundColor(Color(.label))
+                .onTapGesture {
+                    print("Handle like toggle.")
+                }
+            
+            Image(systemName: "bubble.left")
+                .renderingMode(.template)
+                .foregroundColor(Color(.label))
+                .onTapGesture {
+                    print("Handle comment.")
+                }
+            
+        }
+        .padding()
+    }
+    
+    var message: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Group {
+                Text(post.message)
+                    .font(.system(size: 40))
+            }
+        }
+    }
+    
     var body: some View {
         ScrollView {
-            NavigationView {
-                VStack {
-                    Text("Welcome User")
-                        .padding(50)
-                        .font(.largeTitle)
-                        .padding(.bottom, 20)
-                    
-                    Text("Recent Posts")
-                        .padding(.leading,20)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                    
-                    VStack {
-                        Image("profile")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .accessibilityIdentifier("profile")
-                        Spacer()
-                        
-                    VStack {}
-                    }
-                }
+            VStack {
+                welcome
+                
+                header
+                
+                Image("Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipped()
+                    .frame(width: 300, height: 300)
+                
+                actionButtons
+                
+                message
+                
+                Spacer()
             }
         }
     }
 }
-    
 
 struct PostsView_Previews: PreviewProvider {
     static var previews: some View {
-        PostsView()
+        PostsView(post: Post(id: 0, username: "Archie", message: "woof", imgUrl: "", likes: 3))
     }
 }
